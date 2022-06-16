00eba5e7ac5a99cb39f1be9bbd02113a3f40de2d
Fernando Padilla <fernandoslort@gmail.com>
2022-06-16 10:57:51 -0400

    update: app/models.py

--- app/models.py
+++ app/models.py
@@ -2,10 +2,17 @@ from django.db import models
 
 # Create your models here.
 
-
 class UserCustom(models.Model):
     name = models.CharField(max_length=50)
     lastName = models.CharField(max_length=50)
     age = models.IntegerField()
     address = models.CharField(max_length=50)
     phone = models.CharField(max_length=50)
+
+
+class Supplier(models.Model):
+    razon = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField()
+    create_at = models.DateTimeField(auto_now_add=True)
