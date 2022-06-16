b0b373345e0456f99fcaf4da2a5ac8d5216ceff2
Fernando Padilla <fernandoslort@gmail.com>
2022-06-16 10:58:45 -0400

    update:app/views.py

--- app/views.py
+++ app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from .models import UserCustom
+
+from .models import UserCustom, Supplier
+
+from app.forms import FormSupplier
 
 # Create your views here.
 
@@ -15,3 +18,24 @@ def page_user(request):
     return render(request, 'users.html', {
         'users': users
     })
+
+
+def page_registerSupplier(request):
+
+    if request.method == 'POST':
+
+        razon = request.POST['razon']
+        address = request.POST['address']
+        phone = request.POST['phone']
+        email = request.POST['email']
+
+        supplier = Supplier(
+            razon=razon,
+            address=address,
+            phone=phone,
+            email=email
+        )
+
+        supplier.save()
+
+    return render(request, 'register_supplier.html')
